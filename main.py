0ddb3e62bdc43760a9f8e28ddc01145c2ff729dd
vishu <vishupatel716@gmail.com>
2021-09-27 23:36:13 +0530

    commit

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired, NumberRange
 import requests
-
+import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///movies.db'
 db = SQLAlchemy(app)
 
-API_KEY = '9ee5dd6e9ecd62ccf7ed6b5b21ffe19a'
+API_KEY = os.environ.get('MOVIES_API')
 
 
 
@@ -38,17 +38,7 @@ class Add(FlaskForm):
     add = SubmitField('Add')
 
 
-# new_movie = Movies(
-#         title="Phone Booth",
-#         year=2002,
-#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#         rating=7.3,
-#         ranking=10,
-#         review="My favourite character was the caller.",
-#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-#     )
-# db.session.add(new_movie)
-# db.session.commit()
+
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
